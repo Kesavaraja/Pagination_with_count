c5da9e75f8b0cb89ee663b9088a88d80377c9685
Kesavaraja Krishnan <k7raja@live.com>
2022-09-09 17:11:22 +0530

    render details updated.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import PaginationApp from './lib/components/paginationApp.js';
 import * as style from './assets/styles.css';
 
-const container = document.getElementById('root');
-
-// Create a root.
-const root = ReactDOM.createRoot(container);
-
-// Initial render
-root.render(<PaginationApp />);
-
-// During an update, there is no need to pass the container again
-root.render(<PaginationApp />);
+ReactDOM.render(
+    <React.StrictMode>
+      <PaginationApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
